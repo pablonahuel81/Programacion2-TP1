e15834fcbf6794b22fe225421cc683a27e75b5f3
admin1parana <admin1parana@DESKTOP-C79KU2D>
2025-08-17 18:59:43 -0300

    Ejercicio TP programacion 2

---
+++ src/Ejercicio3/StringIntDoubleBoolean.java
@@ -0,0 +1,23 @@
+
+package Ejercicio3;
+
+public class StringIntDoubleBoolean {
+    
+    public static void main(String[] args) {
+        
+        String nombre = "Pablo Nahuel";
+        
+        int edad = 33;
+        
+        double altura = 1.89;
+        
+        boolean estudiante = true;
+        
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("Es estudiante: " + estudiante);
+                
+    }
+    
+}
