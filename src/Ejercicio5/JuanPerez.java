c67c23bca3134cd3e4ac43cb5f7ec3b2d6170dfa
admin1parana <admin1parana@DESKTOP-C79KU2D>
2025-08-18 20:33:48 -0300

    Ejercicio TP programacion 2

---
+++ src/Ejercicio5/JuanPerez.java
@@ -0,0 +1,20 @@
+
+package Ejercicio5;
+
+
+public class JuanPerez {
+    
+    public static void main(String[] args) {
+        
+        String nombre = "Juan Perez";
+        
+        int edad = 30;
+        
+        String direccion = "calle falsa 123";
+        
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(direccion);
+        
+    }
+}
